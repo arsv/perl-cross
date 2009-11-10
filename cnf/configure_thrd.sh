6093eb352f127c72d6fc80e8cfcea5dd645d8c9c
Alex Suykov <axs@ukr.net>
2009-11-10 14:12:00 +0200

    all _r functions, still untested and buggy

--- cnf/configure_thrd.sh
+++ cnf/configure_thrd.sh
@@ -6,7 +6,7 @@
 # Both presence *and* prototype for the function are checked here,
 # with prototype encoded (almost) the same way relevant constants from
 # config.h use:
-# 	[A-Z]_[A-Z]+
+# 	[A-Za-z]_[A-Za-z]+
 # Each letter mean one type; first, return type, then arguments
 #	I_BBW	int foo(char*, char*, size_t)
 #	V_HI	void bar(FILE*, int)
@@ -19,6 +19,14 @@ type_V='void'
 type_H='FILE*'
 type_W='size_t'
 type_Z='double'
+type_E='int*'
+type_U='unsigned long'
+type_L='long int'
+type_i='int*'
+type_l='long*'
+type_t='int32_t*'
+type_s='socklen_t'
+type_u='uint32_t'
 
 # There are also four special letters: T S D and R. Types for these are
 # specified for each test separately (usually that's pointer to some struct)
@@ -48,11 +56,11 @@ function hasfuncr {
 	msg "Checking which prototype $w has"
 	# The following "real" prototype checks may return false positives
 	# if none of included headers declares prototype for $w. To mend this, we
-	# first check if ostesibly incorrect prototype 'D_Z' will return false.
-	# Note: it is assumed none of the functions being tested has D_Z prototype
+	# first check if ostesibly incorrect prototype 'V_Z' will return false.
+	# Note: it is assumed none of the functions being tested has V_Z prototype
 	# (which is likely true, give the value of $type_Z)
-	if hasfuncr_proto "$w" "$i" 'D_Z' "$@"; then
-		msg "\toops, no function should have D_Z prototype"
+	if hasfuncr_proto "$w" "$i" 'V_Z' "$@"; then
+		msg "\toops, no function should have V_Z prototype"
 		msg "\tassuming there's none defined and the function is not usable"
 		setvar "$D" 'undef'
 		return 1;
@@ -63,7 +71,7 @@ function hasfuncr {
 			return 0;
 		fi
 	done
-	msg "\tfailed, assuming $w in unusable"
+	msg "\tfailed, assuming $w is unusable"
 	setvar "$D" 'undef'
 	return 1
 }
@@ -125,12 +133,69 @@ check hasfuncr asctime_r 'time.h' 'B_SB B_SBI I_SB I_SBI' 'const struct tm*'
 check hasfuncr crypt_r 'sys/types.h stdio.h crypt.h' 'B_CCS B_CCD' 'struct crypt_data*'
 check hasfuncr ctermid_r 'sys/types.h stdio.h' 'B_B'
 check hasfuncr endpwent_r 'sys/types.h stdio.h pwd.h' 'I_H V_H'
-check hasfuncr getgrent_r 'sys/types.h stdio.h grp.h' 'I_SBWR I_SBIR S_SBW S_SBI I_SBI I_SBIH' 'struct group*' 'struct group**'
+check hasfuncr getgrent_r 'sys/types.h stdio.h grp.h' 'I_SBWR I_SBIR S_SBW S_SBI I_SBI I_SBIH' \
+	'struct group*' 'struct group**'
 check hasfuncr endgrent_r 'sys/types.h stdio.h grp.h' 'I_H V_H'
-check hasfuncr getgrgid_r 'sys/types.h stdio.h grp.h' 'I_TSBWR I_TSBIR I_TSBI S_TSBI' 'gid_t' 'struct group*' 'struct group**'
-check hasfuncr getgrnam_r 'sys/types.h stdio.h grp.h' 'I_CSBWR I_CSBIR S_CBI I_CSBI S_CSBI' 'struct group*' 'struct group**'
+check hasfuncr getgrgid_r 'sys/types.h stdio.h grp.h' 'I_TSBWR I_TSBIR I_TSBI S_TSBI' \
+	'gid_t' 'struct group*' 'struct group**'
+check hasfuncr getgrnam_r 'sys/types.h stdio.h grp.h' 'I_CSBWR I_CSBIR S_CBI I_CSBI S_CSBI' \
+	'struct group*' 'struct group**'
 check hasfuncr drand48_r 'sys/types.h stdio.h stdlib.h' 'I_ST' 'struct drand48_data*' 'double*'
 check hasfuncr endhostent_r 'sys/types.h stdio.h netdb.h' 'I_D V_D' 'struct hostent_data*'
 check hasfuncr endnetent_r 'sys/types.h stdio.h netdb.h' 'I_D V_D' 'struct netent_data*'
 check hasfuncr endprotoent_r 'sys/types.h stdio.h netdb.h' 'I_D V_D' 'struct protoent_data*'
 check hasfuncr endservent_r 'sys/types.h stdio.h netdb.h' 'I_D V_D' 'struct servent_data*'
+check hasfuncr getgrent_r 'grp.h' 'I_SBWR I_SBIR S_SBW S_SBI I_SBI I_SBIH' 'struct group*' 'struct group**'
+check hasfuncr getgrgid_r 'sys/types.h grp.h' 'I_TSBWR I_TSBIR I_TSBI S_TSBI' 'struct group*' 'struct group**'
+check hasfuncr getgrnam_r 'sys/types.h grp.h' 'I_CSBWR I_CSBIR S_CBI I_CSBI S_CSBI' 'struct group*' 'struct group**'
+check hasfuncr gethostbyaddr_r 'netdb.h' \
+	'I_CWISBWRE S_CWISBWIE S_CWISBIE S_TWISBIE S_CIISBIE S_CSBIE S_TSBIE I_CWISD I_CIISD I_CII I_TsISBWRE' \
+	'const void*' 'struct hostent*' 'struct hostent_data*' 'struct hostent**'
+check hasfuncr gethostbyname_r 'netdb.h' 'I_CSBWRE S_CSBIE I_CSD' \
+	'struct hostent*' 'struct hostent**' 'struct hostent_data*'
+check hasfuncr gethostent_r 'netdb.h' 'I_SBWRE I_SBIE S_SBIE S_SBI I_SBI I_SD'\
+	'struct hostent*' 'struct hostent**' 'struct hostent_data*'
+check hasfuncr getlogin_r 'unistd.h' 'I_BW I_BI B_BW B_BI'
+check hasfuncr getnetbyaddr_r 'netdb.h' \
+	'I_UISBWRE I_LISBI S_TISBI S_LISBI I_TISD I_LISD I_IISD I_uISBWRE'\
+	'in_addr_t' 'struct netent*' 'struct netent_data*' 'struct netent**'
+check hasfuncr getnetbyname_r 'netdb.h' 'I_CSBWRE I_CSBI S_CSBI I_CSD' \
+	'struct netent*' 'struct netent**' 'struct netent_data*'
+check hasfuncr getnetent_r 'netdb.h' 'I_SBWRE I_SBIE S_SBIE S_SBI I_SBI I_SD' \
+	'struct netent*' 'struct netent**' 'struct netent_data*'
+check hasfuncr getprotobyname_r 'netdb.h' 'I_CSBWR S_CSBI I_CSD' \
+	'struct protoent*' 'struct protoent**' 'struct protoent_data*'
+check hasfuncr getprotobynumber_r 'netdb.h' 'I_ISBWR S_ISBI I_ISD' \
+	'struct protoent*' 'struct protoent**' 'struct protoent_data*'
+check hasfuncr getprotoent_r 'netdb.h' 'I_SBWR I_SBI S_SBI I_SD' \
+	'struct protoent*' 'struct protoent**' 'struct protoent_data*'
+check hasfuncr getpwent_r 'pwd.h' 'I_SBWR I_SBIR S_SBW S_SBI I_SBI I_SBIH' \
+	'struct passwd*' 'struct passwd**'
+check hasfuncr getpwnam_r 'pwd.h' 'I_CSBWR I_CSBIR S_CSBI I_CSBI' \
+	'struct passwd*' 'struct passwd**'
+check hasfuncr getpwuid_r 'sys/types.h pwd.h' 'I_TSBWR I_TSBIR I_TSBI S_TSBI' \
+	 'uid_t' 'struct passwd*' 'struct passwd**'
+check hasfuncr getservbyname_r 'netdb.h' 'I_CCSBWR S_CCSBI I_CCSD' \
+	'struct servent*' 'struct servent**' 'struct servent_data*'
+check hasfuncr getservbyport_r 'netdb.h' 'I_ICSBWR S_ICSBI I_ICSD' \
+	'struct servent*' 'struct servent**' 'struct servent_data*'
+check hasfuncr getservent_r 'netdb.h' 'I_SBWR I_SBI S_SBI I_SD' \
+	'struct servent*' 'struct servent**' 'struct servent_data*'
+check hasfuncr getspnam_r 'shadow.h' 'I_CSBWR S_CSBI' 'struct spwd*' 'struct spwd**'
+check hasfuncr gmtime_r 'time.h' 'S_TS I_TS' 'struct tm*' 'const time_t*'
+check hasfuncr localtime_r 'time.h' 'S_TS I_TS' 'struct tm*' 'const time_t*'
+check hasfuncr random_r 'stdlib.h' 'I_iS I_lS I_St' 'struct random_data*'
+check hasfuncr readdir64_r 'stdio.h dirent.h' 'I_TSR I_TS' 'DIR*' 'struct dirent64*' 'struct dirent64**'
+check hasfuncr readdir_r 'stdio.h dirent.h' 'I_TSR I_TS' 'DIR*' 'struct dirent*' 'struct dirent**'
+check hasfuncr setgrent_r 'grp.h' 'I_SBWR I_SBIR S_SBW S_SBI I_SBI I_SBIH' 'struct group*' 'struct group**'
+check hasfuncr sethostent_r 'netdb.h' 'I_ID V_ID' 'struct hostent_data*'
+check hasfuncr setlocale_r 'locale.h' 'I_ICBI'
+check hasfuncr setnetent_r 'netdb.h' 'I_ID V_ID' 'struct netent_data*'
+check hasfuncr setprotoent_r 'netdb.h' 'I_ID V_ID' 'struct protoent_data*'
+check hasfuncr setpwent_r 'pwd.h' 'I_H V_H'
+check hasfuncr setservent_r 'netdb.h' 'I_ID V_ID' 'struct servent_data*'
+check hasfuncr srand48_r 'stdlib.h' 'I_LS' 'struct drand48_data*'
+check hasfuncr srandom_r 'stdlib.h' 'I_TS' '' 'struct random_data*'
+check hasfuncr strerror_r 'string.h' 'I_IBW I_IBI B_IBW'
+check hasfuncr tmpnam_r 'stdio.h' 'B_B'
+check hasfuncr ttyname_r 'stdio.h unistd.h' 'I_IBW I_IBI B_IBI'
