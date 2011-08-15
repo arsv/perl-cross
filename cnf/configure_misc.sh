8e57533e839d39f21f166478d76bcbb8c9d5c53e
Alex Suykov <axs@ukr.net>
2011-08-15 16:43:39 +0300

    lddlflags default value

---
+++ cnf/configure_misc.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Some final tweaks that do not fit in any other file
+
+# Use $ldflags as default value for $lddlflags, together with whatever
+# hints provided, but avoid re-setting anyting specified in the command line
+if [ -n "$ldflags" -a "$x_lddlflags" != "user" ]; then
+	msg "Checking which flags from \$ldflags to move to \$lddlflags"
+	for f in $ldflags; do 
+		case "$f" in
+			-L*|-R*|-Wl,-R*)
+				msg "\tadded $f"
+				appendvar 'lddlflags' "$f"
+				;;
+		esac
+	done
+fi
