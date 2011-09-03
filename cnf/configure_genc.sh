3f39d6c6bc8319b31243fe11910a2b94ad80e1ec
Alex Suykov <axs@ukr.net>
2011-09-03 22:40:07 +0300

    ccdefines in _genc

--- cnf/configure_genc.sh
+++ cnf/configure_genc.sh
@@ -222,6 +222,7 @@ default cccdlflags '-fPIC'
 default ccdlflags '-Wl,-E'
 default ccflags '-fno-strict-aliasing -pipe'
 default ccflags_uselargefiles ''
+default ccdefines ''
 default ccname
 default ccsymbols
 default ccversion
