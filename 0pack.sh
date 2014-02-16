a4fcb1ef2411f6f2d1f593490d5d182849c505ce
Alex Suykov <alex.suykov@gmail.com>
2014-02-16 21:20:00 +0200

    github support files

---
+++ 0pack.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# perl-cross, being an overlaid changeset, is not particulary
+# git- or github-friendly.
+#
+# To make it look nice on Github, it must carry README, Copying
+# and have a plain directory layout. When deploying it onto a perl
+# source tree, it's better not to have said files.
+#
+# Berlios versions were packed to match relevant perl directory
+# name, and it worked well. So the idea here is to keep
+# Github-friendly directory structure for Github, and have this
+# script to pack the files into an easily-applicable tarball
+# for deploying.
+#
+# Tar with --xform option support is expected.
+
+PV=5.8.2
+
+tar -cf perl-cross.tar\
+	--exclude README.rst\
+	--exclude Artistic\
+	--exclude Copying\
+	--exclude 0pack.sh\
+	--exclude perl-cross.tar\
+	--xform "s!^!perl-$PV/!"\
+	*
---
+++ README.rst
@@ -0,0 +1,3 @@
+This is detached git repo for perl-cross project. It is not complete yet;
+please refer to the original BerliOS page http://perlcross.berlios.de
+for releases and documentation.
