ce13bcbccc8017d1d208cd7e3e7a49126cb499c3
Alex Suykov <alex.suykov@gmail.com>
2014-10-08 09:58:30 +0300

    byteorder test for ppc

--- cnf/configure_byte.sh
+++ cnf/configure_byte.sh
@@ -13,23 +13,31 @@ function byteorder {
 	elif [ -n "$uvsize" ]; then
 		result "unknown"
 		msg "\tcan't check byte order with uvsize=$uvsize"
-		return 1
+		exit 1
 	else
 		result "unknown"
 		msg "\tcan't check byte order without known uvsize"
-		return 1
+		exit 1
 	fi
 
 	if try_compile; then
-		if try_objdump -j .data -s; then
+		# Most targets use .data but PowerPC has .sdata instead
+		if try_objdump -j .data -j .sdata -s; then
 			byteorder=`grep '11' try.out | grep '44' | sed -e 's/  .*//' -e 's/[^1-8]//g' -e 's/\([1-8]\)\1/\1/g'`
-			result "$byteorder"
-			return 0
+			if [ -n "$byteorder" ]; then
+				result "$byteorder"
+				return 0
+			else
+				msg "\tcannot determine byteorder for this target"
+				msg "\tplease supply -Dbyteorder= in the command line"
+				msg "\tcommon values: 1234 for 32bit little-endian, 4321 for 32bit big-endian"
+				exit 1
+			fi
 		fi
 	fi
 
 	result 'unknown'
-	return 1
+	exit 1
 }
 
 check byteorder
