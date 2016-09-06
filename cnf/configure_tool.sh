f04d3ab2a07a7f9330dfbbf088b2a3b11c32a367
Alex Suykov <alex.suykov@gmail.com>
2016-09-07 01:43:44 +0300

    tweak/simplify cc type and version detection
    Also drop clauses for icc and Sun cc.

--- cnf/configure_tool.sh
+++ cnf/configure_tool.sh
@@ -83,75 +83,39 @@ else
 fi
 
 setvar 'cpp' "$cc -E"
+log
 
-# some more info on the compiler
-mstart "Checking for GNU cc in disguise and/or its version number"
-if nothinted 'gccversion'; then
-	try_start
-	try_cat <<END
-#if defined(__GNUC__) && !defined(__INTEL_COMPILER)
-#ifdef __VERSION__
-VERSION __VERSION__
-#endif
-#endif
-END
-	try_dump
-	if not run $cc $ccflags -E try.c > try.out 2>>$cfglog; then
-		result "definitely not gcc"
+mstart "Trying to guess what kind of compiler \$cc is"
+if nothinted 'cctype'; then
+	if run $cc --version >try.out 2>&1; then
+		_cl=`head -1 try.out`
+	elif run $cc -V >try.out 2>&1; then
+		_cl=`head -1 try.out`
 	else
-		# a bit paranoid here, in case some non-gnu compiler will decide to
-		# output something unexpected
-		_r=`grep -v '^#' try.out | grep . | head -1 | grep '^VERSION' | sed -e 's/VERSION //' -e 's/"//g'`
-		if [ -n "$_r" ]; then
-			setvar 'gccversion' "$_r"
+		_cl=''
+	fi
+
+	try_dump_out
+	if [ -z "$_cl" ]; then
+		result 'unknown'
+	else case "$_cl" in
+		*\(GCC\)*)
+			_cv=`echo "$_cl" | sed -e 's/.*(GCC) //' -e 's/ .*//g'`
 			setvar 'cctype' 'gcc'
-			result "gcc ver. $_r"
-		else
-			result "probably not gcc"
-		fi
-	fi
-fi
-
-if [ -z "$cctype" ]; then
-	mstart "Trying to guess what kind of compiler \$cc is"
-	if nothinted 'cctype'; then
-		if $cc -V >try.out 2>&1; then
-			_cl=`head -1 try.out`
-		elif $cc --version >try.out 2>&1; then
-			_cl=`head -1 try.out`
-		else
-			_cl=''
-		fi
-
-		if [ -n "$_cl" ]; then
-			case "$_cl" in
-				*\(GCC\)*)
-					setvar 'cctype' 'gcc'
-					result 'GNU cc (probably defunct)'
-					;;
-				*"Intel(R) C++ Compiler"*|*"Intel(R) C Compiler"*)
-					setvar 'cctype' 'icc'
-					setvar ccversion `$cc --version | sed -n -e 's/^icp\?c \((ICC) \)\?//p'`
-					result 'Intel cc'
-					;;
-				*" Sun "*"C"*)
-					setvar 'cctype' 'sun'
-					result 'Sun cc'
-					;;
-				*)
-					result 'unknown'
-					;;
-			esac
-		else
-			_cc=`echo "$cc" | sed -e 's!.*/!!' -e "s/^$target-//" | sed -e 's/-[0-9][0-9.a-z]*$//'`
-			if [ -n "$_cc" -a "$_cc" != 'cc' ]; then
-				setvar 'cctype' "$_cc"
-				result "$_cc"
-			else
-				result 'unknown'
-			fi
-		fi
-	fi
+			setvar 'ccversion' "$_cv"
+			setvar 'gccversion' "$_cv"
+			result "gcc $_cv"
+			;;
+		clang*)
+			_cv=`echo "$_cl" | sed -e 's/.*version //' -e 's/ .*//'`
+			setvar 'cctype' 'clang'
+			setvar 'ccversion' "$_cv"
+			result "clang $_cv"
+			;;
+		*)
+			result 'unknown'
+			;;
+	esac; fi
 fi
 
 mstart "Checking whether $cc is a C++ compiler"
