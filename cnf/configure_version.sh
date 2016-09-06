aa52905fe92c13386aa0d69bf8f919ea95e06b8e
Alex Suykov <alex.suykov@gmail.com>
2016-09-06 18:40:11 +0300

    simplify perl version reporting

--- cnf/configure_version.sh
+++ cnf/configure_version.sh
@@ -3,11 +3,10 @@
 # setverpart name NAME
 setverpart() {
 	_v=`grep '#define' patchlevel.h | grep "$2" | head -1 | sed -r -e "s/#define $2\s+//" -e "s/\s.*//"`
-	msg "	$1=$_v"
 	setvar $1 "$_v"
 }
 
-msg "Getting the current patchlevel..."
+mstart "Checking perl version"
 setifndef package 'perl5'
 if [ -r patchlevel.h ]; then
 	setverpart revision PERL_REVISION
@@ -18,9 +17,9 @@ if [ -r patchlevel.h ]; then
 	setverpart api_subversion PERL_API_SUBVERSION
 
 	v=`egrep ',"(MAINT|SMOKE)[0-9][0-9]*"' patchlevel.h|tail -1|sed 's/[^0-9]//g'`
-	msg "	patchlevel=$v"
 	setvar perl_patchlevel "$v"
 else
+	result "unknown"
 	die "No patchlevel.h found, aborting"
 fi
 
@@ -30,8 +29,6 @@ if [ "$perl_patchlevel" != '' -a "$perl_patchlevel" != '0' ]; then
 	perl_patchlevel=`echo $perl_patchlevel | sed 's/.* //'`
 	version_patchlevel_string="$version_patchlevel_string patch $perl_patchlevel"
 fi
-
-msg "	You have $package $version_patchlevel_string"
 
 setvar PERL_CONFIG_SH true
 setvar PERL_REVISION $revision
@@ -46,27 +43,27 @@ setvar api_versionstring "$api_revision.$api_version.$api_subversion"
 # Detect cperl to apply cperl-specific settings, here and in other files as well
 # Note $base points to cnf/ not the top-level source dir.
 
-mstart "Checking perl variant"
 if [ -f $base/../pod/perlcperl.pod ]; then
 	setvaru usecperl define '' # force this into config.sh
 	setvar package 'cperl'
 	setvar perlname 'cperl'
 	setvar spackage 'cPerl'
-	result "cperl"
 else
 	setvar package 'perl5'
 	setvar perlname 'perl'
 	setvar spackage 'Perl5'
-	result "mainline"
 fi
+
+version="$PERL_REVISION.$PERL_VERSION.$PERL_SUBVERSION"
+packver="$package-$version"
+
+result "$packver"
 
 # Check for patches. Missing patchset indicates unsupported version,
 # and almost certain build failure.
 
-version="$PERL_REVISION.$PERL_VERSION.$PERL_SUBVERSION"
-
-if [ ! -d "$base/diffs/$package-$version" ]; then
-	msg "No patchset found for $package-$version in $base/diffs"
+if [ ! -d "$base/diffs/$packver" ]; then
+	msg "No patchset found for $packver in $base/diffs"
 	msg "This perl version is probably not supported by perl-cross"
 	exit 255
 fi
