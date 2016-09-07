c1f884c59977693d78300a1dfd57d64c323781db
Alex Suykov <alex.suykov@gmail.com>
2016-09-07 15:24:03 +0300

    move -g/-DEBUGGING setup into _tool
    That's toolchains setup after all.

--- cnf/configure_tool.sh
+++ cnf/configure_tool.sh
@@ -267,3 +267,38 @@ if nothinted 'osname'; then
 			;;
 	esac
 fi
+
+# Handle -DEBUGGING
+
+mstart "Checking whether to enable -g"
+case "$DEBUGGING" in
+	-g|both|define)
+   		case "$optimize" in
+			*-g*)
+				result "already enabled" ;;
+			*)
+				appendvar optimize "-g"
+				result "yes" ;;
+   		esac ;;
+	none|undef)
+		case "$optimize" in
+			*-g*) setvar optimize "`echo $optimize | sed -e 's/-g ?//'`" ;;
+   		esac
+		result "no" ;;
+	*)
+		result "no" ;;
+esac
+
+mstart "Checking whether to use -DDEBUGGING"
+case "$DEBUGGING" in
+	both|define)
+		case "$ccflags" in 
+			*-DDEBUGGING*)
+				result "already there" ;;
+			*)
+				appendvar optimize '-DDEBUGGING'
+				result "yes" ;;
+		esac ;;
+	*)
+		result "no" ;;
+esac
