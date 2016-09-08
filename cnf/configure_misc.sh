f99230c9f29bc2ea051b52c8c4502d86eb4bae01
Alex Suykov <alex.suykov@gmail.com>
2016-09-09 01:44:12 +0300

    single-set: configure_misc

---
+++ cnf/configure_misc.sh
@@ -0,0 +1,391 @@
+# Misc settings we do not test for.
+
+# Configured-by; disabled, no point in leaking usernames
+define cf_by ''
+define cf_email ''
+define cf_time ''
+# No need to leak these either
+define perladmin 'nobody@nowhere.land'
+define osvers 'current'
+define config_arg0 ''
+define config_argc 0
+define config_args ''                                                        
+define myarchname ''
+define mydomain ''
+define myhostname ''
+define myuname ''
+
+# "use MakeMaker direct LD Library Test"
+# see cpan/ExtUtils-MakeMaker/lib/ExtUtils/MakeMaker/Liblist/Kid.pm
+# and resp. patch for reasons
+define usemmldlt 'define'
+
+# Used by modules?
+define dlext 'so'
+define dlsrc 'dl_dlopen.xs'
+
+# These are sometimes used in perl-cross, and sometimes also affect modules
+# Some *must* be "false" instead of "undef"! See configure_args.sh
+define use5005threads 'undef'
+define use64bitall 'undef'
+define use64bitint 'undef'
+define usecbacktrace 'undef'
+define usecrosscompile 'undef'
+define usedevel 'undef'
+define usedl 'define'
+define usedtrace 'undef'
+define usefaststdio 'undef'
+define useithreads 'undef'
+define usekernprocpathname 'undef'
+define uselargefiles 'define'
+define uselongdouble 'undef'
+define usemallocwrap 'define'
+define usemorebits 'undef'
+define usemultiplicity 'undef'
+define usemymalloc 'n'
+define usenm 'false'
+define usensgetexecutablepath 'undef'
+define useopcode 'false'
+define useperlio 'define'
+define useposix 'true'
+define usequadmath 'undef'
+define usereentrant 'undef'
+define userelocatableinc 'undef'
+define usesfio 'false'
+define useshrplib 'false'
+define usesitecustomize 'undef'
+define usesocks 'undef'
+define usethreads 'undef'
+define useversionedarchname 'undef'
+define usevfork 'false'
+
+# These are important but we don't test them
+define startsh '#!/bin/sh'
+define spitshell 'cat'
+define d_Gconvert 'sprintf((b),"%.*g",(n),(x))'
+define d_c99_variadic_macros 'undef'
+define d_modfl_pow32_bug 'undef'
+define direntrytype 'struct dirent'
+define drand01 'drand48()'
+define fflushNULL 'define'
+define fflushall 'undef'
+define freetype 'void'
+define malloctype 'void*'
+define mmaptype 'void *'
+define modetype 'mode_t'
+define need_va_copy 'define'
+define o_nonblock 'O_NONBLOCK'
+define old_pthread_create_joinable 'PTHREAD_CREATE_JOINABLE'
+define prototype 'define'
+define randbits 48
+define randseedtype 'long'
+define rd_nodata -1
+define sched_yield 'sched_yield()'
+define seedfunc 'srand48'
+define socksizetype 'socklen_t'
+define vaproto 'define'
+define groupstype 'gid_t'
+define h_fcntl 'false'
+define h_sysfile 'true'
+define ldlibpthname 'LD_LIBRARY_PATH'
+define pidtype 'pid_t'
+define selectminbits '32'
+define selecttype 'fd_set *'
+define shmattype 'void *'
+define st_ino_sign 1
+define st_ino_size 4 # XXX 8 most modern Linux systems
+define d_open3 'define'
+define d_safebcpy 'define'
+define d_safemcpy 'undef'
+define d_sanemcmp 'define'
+define d_casti32 'undef'
+define d_castneg 'define'
+define d_static_inline 'undef'
+define d_stdstdio 'undef'
+define d_stdio_cnt_lval 'undef'
+define d_stdio_ptr_lval 'undef'
+define d_stdio_ptr_lval_nochange_cnt 'undef'
+define d_stdio_ptr_lval_sets_cnt 'undef'
+define d_stdiobase 'undef'
+define d_vprintf 'define'
+define d_charvspr 'undef'
+define d_eofnblk 'define'
+define d_printf_format_null 'define'
+define d_const 'define'
+define d_csh 'undef'
+define d_suidsafe 'undef'
+define d_dosuid 'undef'
+define d_flexfnam 'define'
+define d_phostname 'undef'
+define d_bsdgetpgrp 'undef'
+define d_bsdsetpgrp 'undef'
+define d_shmatprototype 'define'
+define d_mymalloc 'undef'
+define d_msg 'undef'
+define d_strctcpy 'define'
+define d_pseudofork 'undef'
+define d_dlsymun 'undef'
+define d_fcntl_can_lock 'undef' # XXX it can?
+define d_sprintf_returns_strlen 'define'
+define d_u32align 'undef'
+define d_dir_dd_fd 'undef'
+define d_old_pthread_create_joinable 'undef'
+define d_oldpthreads 'undef'
+define d_nv_zero_is_allbits_zero 'define'
+define d_stdio_stream_array 'undef'
+define d_ctime_r 'undef' # XXX thrd?
+define d_faststdio 'undef'
+define d_libm_lib_version 'undef'
+define d_localtime_r_needs_tzset 'undef'
+define signal_t 'void'
+
+define d_gethostprotos 'define'
+define d_getservprotos 'define'
+define d_getnetprotos 'define'
+define d_getprotoprotos 'define'
+
+define d_dbminitproto 'define'
+define d_dbminitproto 'define'
+define d_drand48proto 'define'
+define d_flockproto 'define'
+define d_lseekproto 'define'
+define d_modflproto 'define'
+define d_sbrkproto 'define'
+define d_shmatprototype 'define'
+define d_sockatmarkproto 'define'
+define d_sresgproto 'define'
+define d_sresuproto 'define'
+define d_syscallproto 'define'
+define d_telldirproto 'define'
+define d_usleepproto 'define'
+define d_usleepproto 'define'
+
+define d_procselfexe 'undef'
+define procselfexe '"/proc/self/exe"'
+
+# These should be in modules?
+define netdb_hlen_type 'socklen_t'
+define netdb_host_type 'const void *'
+define netdb_name_type int
+define netdb_net_type 'uint32_t'
+define db_hashtype 'unsigned int'
+define db_prefixtype 'size_t'
+define db_version_major ''
+define db_version_minor ''
+define db_version_patch ''
+define d_ndbm 'undef'
+define d_ndbm_h_uses_prototypes 'define'
+define d_gdbm_ndbm_h_uses_prototypes 'define'
+define d_gdbm_ndbm_h_uses_prototypes 'undef'
+define d_gdbmndbm_h_uses_prototypes 'define'
+define d_gdbmndbm_h_uses_prototypes 'undef'
+
+# Non-testable printf formats; must be hinted
+define d_PRIEUldbl 'undef'
+define d_PRIFUldbl 'undef'
+define d_PRIGUldbl 'undef'
+define d_PRIXU64 'undef'
+define d_PRId64 'undef'
+define d_PRIeldbl 'undef'
+define d_PRIfldbl 'undef'
+define d_PRIgldbl 'undef'
+define d_PRIi64 'undef'
+define d_PRIo64 'undef'
+define d_PRIu64 'undef'
+define d_PRIx64 'undef'
+define d_SCNfldbl 'undef'
+define sGMTIME_max '2147483647'
+define sGMTIME_min '-2147483648'
+define sLOCALTIME_max '2147483647'
+define sLOCALTIME_min '-2147483648'
+define sPRIEUldbl '"LE"'
+define sPRIFUldbl '"LF"'
+define sPRIGUldbl '"LG"'
+define sPRIXU64 '"LX"'
+define sPRId64 '"Ld"'
+define sPRIeldbl '"Le"'
+define sPRIfldbl '"Lf"'
+define sPRIgldbl '"Lg"'
+define sPRIi64 '"Li"'
+define sPRIo64 '"Lo"'
+define sPRIu64 '"Lu"'
+define sPRIx64 '"Lx"'
+define sSCNfldbl '"Lf"'
+define nvEUformat '"E"'
+define nvFUformat '"F"'
+define nvGUformat '"G"'
+define nveformat '"e"'
+define nvfformat '"f"'
+define nvgformat '"g"'
+define uvXUformat '"lX"'
+define uvoformat '"lo"'
+define uvuformat '"lu"'
+define uvxformat '"lx"'
+define uidformat '"lu"'
+define ivdformat '"ld"'
+define gidformat '"lu"'
+
+# These probably affect something in some cases but we don't test them
+define afs 'false'
+define afsroot '/afs'
+define baserev 5.0
+define bin_ELF 'define' # XXX safe default is 'undef'
+define castflags 0
+define ccsymbols ''
+define charbits 8
+define cpp_stuff 42
+define cppccsymbols
+define eagain 'EAGAIN'
+define ebcdic 'undef'
+define gccosandvers ''
+define gnulibc_version '' # not tested for
+define mips_type ''
+define multiarch 'undef'
+define perl_static_inline 'static'
+define startperl "$sharpbang$perlpath"
+define phostname hostname
+define stdchar char
+define stdio_base
+define stdio_bufsiz
+define stdio_cnt
+define stdio_filbuf
+define stdio_ptr
+define stdio_stream_array
+define d_lc_monetary_2008 'undef'
+define so 'so'
+define sharpbang '#!'
+
+# Non-toolchain commands; not used by perl-cross
+define ansi2knr
+define aphostname /bin/hostname
+define awk awk
+define bash bash
+define bison bison
+define byacc byacc
+define cat cat
+define chgrp chgrp
+define chmod chmod
+define chown chown
+define comm comm
+define contains grep
+define cp cp
+define cpio cpio
+define csh '' # do keep this disabled
+define date date
+define dtrace
+define echo echo
+define egrep egrep
+define emacs
+define expr expr
+define find
+define flex
+define full_ar ar
+define full_csh csh
+define full_sed sed
+define gmake gmake
+define grep grep
+define groupcat 'cat /etc/group'
+define gzip gzip
+define hostcat 'cat /etc/hosts'
+define inews # unused
+define issymlink "test -h"
+define ksh
+define less less
+define lint # unused
+define ln 'ln'
+define lns "$ln -s"
+define lp
+define lpr
+define ls ls
+define mail
+define mailx
+define make make
+define mkdir mkdir # unused
+define more more
+define mv
+define nroff nroff
+define pager less
+define passcat 'cat /etc/passwd'
+define pg pg # unused
+define pmake # unused
+define pr # unused
+define rm rm # unused
+define rm_try # unused
+define rmail # unused
+define runnm false # unused
+define sed sed # unused
+define sendmail
+define sh /bin/sh
+define shar # unused
+define sleep # unused
+define smail # unused
+define sort sort
+define tail
+define tar
+define tee
+define test test
+define touch touch
+define tr tr
+define trnl '\n'
+define troff
+define uuname
+define vi
+define zcat
+define zip zip
+define tbl
+define uname uname
+define uniq uniq
+define yacc yacc
+define yaccflags ''
+
+# These make sense in mainline perl build system but not in perl-cross
+define archname64
+define archobjs
+define ccname
+define compress
+define cryptlib
+define eunicefix ':'
+define firstmakefile 'Makefile'
+define from :
+define gccansipedantic
+define hint 'default'
+define ignore_versioned_solibs
+define incpath
+define initialinstalllocation ''
+define libc
+define libsfiles
+define libsfound
+define libspath
+define lkflags
+define locincpth
+define loclibpth
+define make_set_make '#'
+define mallocobj
+define mallocsrc
+define mistrustnm
+define n '-n'
+define orderlib
+define perl
+define perl5
+define run 'false'
+define shrpenv
+define shsharp 'true'
+define sockethdr
+define socketlib
+define src ''
+define strings
+define sysman ''
+define targetsh "$sh"
+define timeincl
+define to ':'
+define usrinc
+define versiononly 'undef'
+define xlibpth ''
+
+# Deprecated symbols
+define exe_ext ''
+define obj_ext .o
+define lib_ext .a
+define path_sep ':'
+define line
+define submit
