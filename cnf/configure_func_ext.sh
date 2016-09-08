45828c46ab5aa0fa152b16f5f62912569369502f
Alex Suykov <alex.suykov@gmail.com>
2016-09-08 20:34:22 +0300

    single-set: configure_func_ext

--- cnf/configure_func_ext.sh
+++ cnf/configure_func_ext.sh
@@ -6,24 +6,24 @@
 checkvar() {
 	require 'cc'
 	mstart "Checking for $2"
-	if nothinted "$1"; then
+	if not hinted "$1" 'found' 'not found'; then
 		try_start
 		try_includes $3
 		try_add "void foo() { };"
 		try_add "int main() { foo($2); return 0; }"
 		try_link
-		resdef 'found' 'not found' "$1"
+		resdef $1 'found' 'not found'
 	fi
 }
 
 checkintdefined() {
 	mstart "Checking whether $2 is defined"
-	if nothinted "$1"; then
+	if not hinted "$1" 'yes' 'no'; then
 		try_start
 		try_includes $3
 		try_add "int i = $2;"
 		try_compile
-		resdef 'yes' 'no' $1
+		resdef $1 'yes' 'no'
 	fi
 }
 
@@ -35,84 +35,41 @@ checkintdefined d_ldbl_dig LDBL_DIG 'limits.h float.h'
 
 mstart "Checking whether closedir is void"
 if [ "$d_closedir" = 'define' ]; then
-	if nothinted "d_void_closedir"; then
+	if not hinted d_void_closedir 'yes' 'no'; then
 		try_start
 		try_includes 'sys/types.h' 'dirent.h'
 		try_add "int main() { return $1($2); }"
-		if try_compile; then
-			setvar 'd_void_closedir' 'undef'
-			result 'no'
-		else
-			setvar 'd_void_closedir' 'undef'
-			result 'yes'
-		fi
+		try_compile
+		resdef d_void_closedir 'yes' 'no'
 	fi
 else
-	setvar 'd_void_closedir' 'undef'
+	define d_void_closedir 'undef'
 	result 'irrelevant'
 fi
 
 mstart "Checking whether prctl supports PR_SET_NAME"
 if [ "$d_prctl" = 'define' ]; then
-	if nothinted 'd_prctl_set_name'; then
+	if not hinted d_prctl_set_name 'yes' 'no'; then
 		try_start
 		try_includes 'sys/prctl.h'
 		try_add "int main (int argc, char *argv[]) {"
 		try_add "	return (prctl (PR_SET_NAME, \"Test\"));"
 		try_add "}"
 		try_compile
-		resdef 'yes' 'no' 'd_prctl_set_name'
+		resdef d_prctl_set_name 'yes' 'no' 
 	fi
 else
-	setvar 'd_prctl_set_name' 'undef'
+	define 'd_prctl_set_name' 'undef'
 	result 'irrelevant'
 fi
 
 mstart "Checking if we're using GNU libc"
-if nothinted 'd_gnulibc'; then
+if not hinted d_gnulibc 'yes' 'no'; then
 	try_start
 	try_add '#include <stdio.h>'
 	try_add "#ifndef __GLIBC__"
 	try_add "#error here"
 	try_add "#endif"
 	try_compile
-	resdef 'yes' 'no' d_gnulibc
+	resdef d_gnulibc 'yes' 'no'
 fi
-
-# Extended test for fpclassify. Linking alone is not enough apparently,
-# the constants must be defined as well.
-
-# checkfpclass d_func func 'includes' D1 D2 D3 ....
-checkfpclass() {
-	_sym=$1
-	_fun=$2
-	_inc=$3
-
-	mstart "Checking whether $_fun() is usable"
-	if nothinted $_sym; then
-		try_start
-		try_includes $_inc
-		try_add "int main(void) { return $_fun(0.0); }"
-		shift; shift; shift;
-
-		for c in $*; do
-			try_add "int v_$c = $c;"
-		done
-
-		if try_link; then
-			setvar "$_sym" 'define'
-			result 'yes'
-		else
-			setvar "$_sym" 'undef'
-			result "no, disabling $_fun()"
-		fi
-	fi
-}
-
-checkfpclass d_fpclassify fpclassify 'math.h' \
-	FP_NAN FP_INFINITE FP_NORMAL FP_SUBNORMAL FP_ZERO
-
-checkfpclass d_fpclass fpclass 'math.h ieeefp.h' \
-	FP_SNAN FP_QNAN FP_NEG_INF FP_POS_INF FP_NEG_INF \
-	FP_NEG_NORM FP_POS_NORM FP_NEG_NORM FP_NEG_DENORM FP_POS_DENORM \
-	FP_NEG_DENORM FP_NEG_ZERO FP_POS_ZERO FP_NEG_ZERO
