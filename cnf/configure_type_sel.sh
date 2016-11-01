d89266c31d10104c6970c94f3feafbaeca6a5e6a
Alex Suykov <alex.suykov@gmail.com>
2016-11-02 00:38:15 +0200

    comment formatting

--- cnf/configure_type_sel.sh
+++ cnf/configure_type_sel.sh
@@ -29,21 +29,22 @@ msg "	IV will be "$ivtype", $ivsize bytes"
 msg "	UV will be "$uvtype", $uvsize bytes"
 msg "	NV will be "$nvtype", $nvsize bytes"
 
-# The following code may be wrong, but there's no way to
-# tell for sure without running on-target tests.
-# And "undef" as a safe default fails op/range.t on some targets.
-# So try to make a guess.
+# The following code may be wrong, but there's no way to tell this
+# for sure without running on-target tests.
+# Using "undef" as a safe default fails op/range.t on some targets.
 #
 # Note that in reality there's not that much choice here, since
-# nvtype is almost invariably an IEEE 754 double (8 bytes) or long double (10 bytes),
-# while uvtype is either 4-byte of 8-byte unsigned integer.
+# nvtype is almost invariably IEEE 754 double (8 bytes) or long double
+# (10 bytes), while uvtype is either 4-byte of 8-byte unsigned integer.
 #
 # Quite surprisingly, perl seems to be content with nv_preserves_uv_bits=0
 # in all cases. However, given the floating-point type selection above,
-# it's seems to be safe to assume preserved bits match IEEE 754 definitions as well.
+# it's seems to be safe to assume preserved bits match IEEE 754 definitions
+# as well.
 #
-# Signedness of uvtype doesn't generally matter, except when it's long double vs 64bit int.
-# However, uvtype should always be unsigned, and the code above makes sure it is.
+# Signedness of uvtype doesn't generally matter, except when it's long double
+# vs 64bit int. However, uvtype should always be unsigned, and the code above
+# makes sure it is.
 mstart "Guessing nv_preserves_uv_bits value"
 if not hinted "nv_preserves_uv_bits"; then
 	case "$nvsize:$uvsize" in
@@ -76,8 +77,8 @@ if not hinted "d_nv_preserves_uv"; then
 	resdef d_nv_preserves_uv "yes" "no"
 fi
 
-# nv_overflows_integers_at is a property of nvtype alone, it doesn't depend on uvtype at all.
-# Assuming IEEE 754 floats here once again.
+# nv_overflows_integers_at is a property of nvtype alone, it doesn't depend on
+# uvtype at all. Assuming IEEE 754 floats here once again.
 mstart "Checking integer capacity of nv"
 if not hinted "nv_overflows_integers_at"; then
 	case "$nvsize" in
