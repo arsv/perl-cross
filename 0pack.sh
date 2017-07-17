bbd088a1591e0f5d8f06585375213554331dd7a1
Alex Suykov <alex.suykov@gmail.com>
2017-07-17 12:26:05 +0300

    pack to 1.1.6

--- 0pack.sh
+++ 0pack.sh
@@ -14,7 +14,7 @@
 # in the package name. In most cases, the bundle will be unpacked
 # with --strip-compontents=1 anyway.
 
-V=1.1.5
+V=1.1.6
 
 tar -zcf perl-cross-$V.tar.gz\
 	--exclude README.md\
