199267d96e1d1983fe36128723008c8c289570d5
Alex Suykov <alex.suykov@gmail.com>
2024-06-10 00:58:17 +0300

    pack to 1.5.3

--- 0pack.sh
+++ 0pack.sh
@@ -14,7 +14,7 @@
 # in the package name. In most cases, the bundle will be unpacked
 # with --strip-compontents=1 anyway.
 
-V=1.5.2
+V=1.5.3
 
 tar -zcf perl-cross-$V.tar.gz\
 	--exclude README.md\
